ab329274390c91b9d87c4e476d45e9a9616164c3
ssemi <ssemi.net@gmail.com>
2014-06-30 16:19:39 +0900

    Update me2crawler.py

--- me2backupPy/me2crawler.py
+++ me2backupPy/me2crawler.py
@@ -7,6 +7,6 @@ me2bb = me2Backup()
 me2bb.timesleep = 1.0;
 me2bb.dataFileType = "xml"
 me2bb.userid = ("ssemi",)
-me2bb.crawlingLastestDate = "2014-06-20"
+#me2bb.crawlingLastestDate = "2014-06-20"
 #me2bb.saveUserData('ssemi', '2014-06-30')
 me2bb.saveUserDataCrawler()
